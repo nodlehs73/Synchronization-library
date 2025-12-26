0c0363e77d46a4fefd872f52e5e4b1f90fcc1796
nodlehs73 <alex.chiper06@gmail.com>
2025-12-26 23:22:45 +0200

    Added rwlock

---
+++ rwlock/rwlock.h
@@ -0,0 +1,43 @@
+#ifndef RWLOCK_H
+#define RWLOCK_H
+
+#include <stdatomic.h>
+
+struct rwlock {
+  atomic_int serving;
+  atomic_int ticket;
+  atomic_int readers;
+};
+
+void rwlock_init (struct rwlock* lock) {
+  atomic_store (&lock->serving, 0);
+  atomic_store (&lock->ticket, 0);
+  atomic_store (&lock->readers, 0);
+}
+
+
+void lock (struct rwlock* lock) {
+  const int ticket_order = atomic_fetch_add (&lock->ticket, 1);
+  
+  while (ticket_order != atomic_load (&lock->serving) && atomic_load (&lock->readers) != 0)
+    ;
+}
+
+void shared_lock (struct rwlock* lock) {
+  const int ticket_order = atomic_fetch_add (&lock->ticket, 1);
+
+  while (ticket_order != atomic_load (&lock->serving)) 
+    ;
+
+  atomic_fetch_add (&lock->readers, 1);
+  atomic_fetch_add (&lock->serving, 1);
+}
+
+void unlock (struct rwlock* lock) {
+  atomic_fetch_add (&lock->serving, 1);
+}
+
+void shared_unlock (struct rwlock* lock) {
+  atomic_fetch_add (&lock->readers, -1); 
+}
+#endif
---
+++ rwlock/rwlock_test.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "rwlock.h"
+
+int counter = 0;
+struct rwlock rwmutex;
+
+void* read_counter (void *data) {
+  int thread_idx = *((int*) data);
+
+  shared_lock (&rwmutex);
+  printf ("READING %d from thread with idx %d\n", counter, thread_idx);
+  
+  shared_unlock (&rwmutex);
+  return NULL;
+}
+
+void* modify_counter (void* data) {
+  int thread_idx = *((int*) data);
+
+  lock (&rwmutex);
+  counter++;
+  
+  unlock (&rwmutex);
+  printf ("MODIFYING COUNTER TO %d WITH THREAD_IDX %d\n", counter, thread_idx);
+
+  return NULL;
+}
+
+pthread_t threads[1000];
+
+int main(int argc, char* argv[]) {
+  rwlock_init (&rwmutex);
+
+  const int READER_CHANCE = atoi (argv[1]);
+  const int NUMBER_THREADS = atoi (argv[2]);
+  
+  for (int i = 0; i < NUMBER_THREADS; ++i) {
+    const int chance = arc4random_uniform (100) + 1;
+    
+    int* thread_idx = (int*) malloc (sizeof (int));
+    *thread_idx = i;
+    
+    printf ("%d %d\n", chance, READER_CHANCE);
+    if (chance <= READER_CHANCE) {
+      pthread_create (&threads[i], NULL, read_counter, (void*) thread_idx);
+    } else {
+      pthread_create (&threads[i], NULL, modify_counter, (void*) thread_idx);
+    }
+  }
+
+  for (int i = 0; i < NUMBER_THREADS; ++i) {
+    pthread_join (threads[i], NULL);
+  }
+}
