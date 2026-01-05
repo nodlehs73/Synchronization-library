8847b2f6b9fbd5469117f14b12e443c02a090aee
and-11 <andreiturcu2005.24@gmail.com>
2026-01-05 21:43:31 +0200

    no futex

--- semaphore/no_futex_custom_semaphore.h
+++ semaphore/no_futex_custom_semaphore.h
@@ -4,18 +4,15 @@
 #include <stdatomic.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include "futex_wrapper.h"
 
 
 struct no_futex_semaphore
 {
-    atomic_int waiters;
     atomic_int limit;
 };
 
 static void no_futex_semaphore_init(struct no_futex_semaphore *sem, const int limit)
 {
-    sem->waiters = 0;
     sem->limit = limit;
 }
 
@@ -31,23 +28,12 @@ static void no_futex_semaphore_wait(struct no_futex_semaphore *sem)   // lock
         {
             return;
         }
-        else 
-        {
-            atomic_fetch_add(&sem->waiters, 1);
-
-            futex_wait(&sem->limit, 0);
-
-            atomic_fetch_add(&sem->waiters, -1);
-        }
     }
 }
 
 static void no_futex_semaphore_post(struct no_futex_semaphore *sem)   // unlock
 {
     atomic_fetch_add(&sem->limit, 1);
-
-    if (atomic_load(&sem->waiters) > 0) 
-        futex_wake(&sem->limit, 1);
 }
 
 
